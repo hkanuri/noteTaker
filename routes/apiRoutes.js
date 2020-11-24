625c55891b85bbd58a973c6b84dcb1cb627caf0b
hkanuri <hkanuri@lululemon.com>
2020-11-24 16:32:43 -0600

    added  new post menthod

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,17 +5,17 @@ const dbJSON = require("../db/db.json");
 module.exports = function (app) {
 
     // write to DB fucntion
-    function updateDB(notes) {
-        notes = JSON.stringify(notes);
-        console.log(notes);
+    // function updateDB(notes) {
+    //     notes = JSON.stringify(notes);
+    //     console.log(notes);
 
-        // write data back to DB
-        fs.writeFileSync("../db/db.json", notes, function (err) {
-            if (err) {
-                return console.log(err)
-            }
-        });
-    }
+    //     // write data back to DB
+    //     fs.writeFileSync("../db/db.json", notes, function (err) {
+    //         if (err) {
+    //             return console.log(err)
+    //         }
+    //     });
+    // }
     //app.get
     app.get("/api/notes", function (req, res) {
 
@@ -28,29 +28,26 @@ module.exports = function (app) {
 
 
     //app.post
-    app.post("/api/notes", function (req, res) {
-        if (notesData.length == 0){
-            req.body.id = "0";
-        } else{
-            req.body.id = JSON.stringify(JSON.parse(notesData[notesData.length - 1].id) + 1);
-        }
+    app.post("/api/notes", function(req, res) {
+
+        let newNote = req.body;
+        let uniqueId = (data.length).toString();
+        console.log(uniqueId);
+        newNote.id = uniqueId;
+        data.push(newNote);
         
-        console.log("req.body.id: " + req.body.id);
+        fs.writeFileSync("./db/db.json", JSON.stringify(data), function(err) {
+            if (err) throw (err);        
+        }); 
 
-        // Pushes Body to JSON Array
-        notesData.push(req.body);
+        res.json(data);    
 
-        // Write notes data to database
-        writeToDB(notesData);
-        console.log(notesData);
-
-        // returns new note in JSON format.
-        res.json(req.body);
     });
 
     //app.delete
 
     app.delete("/api/notes", function (req, res) {
-
+        const id = req.params.id;
+        let newID =0;
     })
 };
